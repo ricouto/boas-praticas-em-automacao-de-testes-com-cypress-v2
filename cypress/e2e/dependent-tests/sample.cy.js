757de229a16f7daeaeaf9c152b722481797185bd
ricouto <ricardocaputo@gmail.com>
2023-12-05 08:14:08 -0300

    Final do curso

--- cypress/e2e/dependent-tests/sample.cy.js
+++ cypress/e2e/dependent-tests/sample.cy.js
@@ -1,17 +1,17 @@
 describe('Dependent tests bad practice', () => {
   beforeEach(() => {
-    cy.visit('http://notes-serverless-app.com')
+    cy.visit('http://notes-serverless-app.com/login')
 
-    cy.get('.navbar-nav a:contains(Login)').click()
+    // cy.get('.navbar-nav a:contains(Login)').click()
 
     cy.get('#email').type(Cypress.env('user_email'))
     cy.get('#password').type(Cypress.env('user_password'), { log: false })
     cy.get('button[type="submit"]').click()
-    
-    cy.contains('h1', 'Your Notes').should('be.visible')
+
+    cy.contains('h1', 'Your Notes', { timeout: 5000 }).should('be.visible')
   })
 
-  it('creates a note', () => {
+  it.skip('creates a note', () => {
     cy.contains('Create a new note').click()
 
     cy.get('#content').type('My note')
@@ -20,7 +20,31 @@ describe('Dependent tests bad practice', () => {
     cy.get('.list-group').should('contain', 'My note')
   })
 
-  it('edits a note', () => {
+  it('creates a note', () => {
+    cy.contains('Create a new note').click()
+
+    cy.get('#content').type('My note')
+    cy.contains('Create').click()
+
+    cy.get('.list-group')
+      .should('contain', 'My note')
+      .click()
+
+    // cy.get('.list-group').contains('My note').click()
+    cy.get('#content').type(' updated')
+    cy.contains('Save').click()
+
+    cy.get('.list-group').should('contain', 'My note updated')
+    cy.get('.list-group:contains(My note updated)')
+      .should('be.visible')
+      .click()
+
+    // cy.get('.list-group').contains('My note updated').click()
+    cy.contains('Delete').click()
+    cy.get('.list-group:contains(My note updated)').should('not.exist')
+  })
+
+  it.skip('edits a note', () => {
     cy.get('.list-group').contains('My note').click()
     cy.get('#content').type(' updated')
     cy.contains('Save').click()
@@ -29,7 +53,7 @@ describe('Dependent tests bad practice', () => {
     cy.get('.list-group:contains(My note updated)').should('be.visible')
   })
 
-  it('deletes a note', () => {
+  it.skip('deletes a note', () => {
     cy.get('.list-group').contains('My note updated').click()
     cy.contains('Delete').click()
 
